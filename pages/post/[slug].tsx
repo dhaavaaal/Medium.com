907117ad321449b4b875e47805f7e72a2b50c282
Dhaval Thakkar <dhavalthakkar1541@gmail.com>
2022-06-21 14:05:38 +0530

    Done with post page.Start with comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,8 @@
+import { GetStaticProps } from "next";
+import PortableText from "react-portable-text";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
 // We need to go and tell NextJS, how it knows which posts exists, how does
 //it know which page to go ahead and pre-build these pages. We can't just
 //magically guess it's gonna know it and not by doing Server Side Rendering(SSR)
@@ -7,14 +10,137 @@ import { sanityClient, urlFor } from "../../sanity";
 //This is gonna allow NextJS to know which routes it should pre-build in
 //advance
 
-const Post = () => {
+//getStaticPaths creates a list of paths and these paths
+//will basically have the folder structure which will be
+//kind of an array
+interface Props {
+  post: Post;
+}
+
+const Post = ({ post }: Props) => {
+  console.log(post);
   return (
     <main>
       <Header />
+
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage)?.url()}
+        alt={post.description}
+      />
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-4xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+          />
+          <p className="font-extralight text-sm">
+            Blog post by{" "}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
     </main>
   );
 };
 
 export default Post;
 
-export const getStaticPaths = async () => {};
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"] {
+  _id,
+  slug {
+    current
+  }  
+}`;
+
+  const posts = await sanityClient.fetch(query);
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0] {
+      _id,
+      _createdAt,
+      title,
+      author-> {
+      name,
+      image
+    },
+    'comments': *[
+      _type == 'comments' &&
+      post._ref == ^._id &&
+      approved == true
+    ],
+    description,
+    mainImage,
+    slug,
+    body
+  }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, //ISR-updates after 60 secs
+  };
+};
+
+//Basically first founded the pages which exist with getStaticPaths()
+//so getStaticPaths() is used to basically find the paths or the pages
+//which exist and then we used getStaticProps() which is the next
+//function that runs which basically populates the information for the
+//page.
